0eb8b5bb15caeb9b3a2b74bbc07880ec09898bcb
Maciej Sprada <macosspr@gmail.com>
2016-07-16 13:41:40 +0200

    ticket-2

--- source/Interfaces/BackendInterface.php
+++ source/Interfaces/BackendInterface.php
@@ -6,4 +6,13 @@ interface BackendInterface
     public function tester1() {
         
     }
+    
+    public function tester2() {
+        $i = -10;
+        while($i < 0) {
+            $i++;
+        }
+        
+        echo $i;
+    }
 }
