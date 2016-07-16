6105e2e7667cc9c549c78b811d9bcb4f401032e3
Maciej Sprada <macosspr@gmail.com>
2016-07-16 15:25:47 +0200

    fdfsf

--- source/AppMainBundle.php
+++ source/AppMainBundle.php
@@ -9,7 +9,7 @@ class AppMainBundle extends Bundle
     
     public function getParent()
     {
-        return 'FOSUserBundleAla';
+        return 'FOSUserBundleAlaOh wchapaniel';
     }
     
     public function getParent2()
