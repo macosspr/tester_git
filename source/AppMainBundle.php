c7ce53b5267ac65a95ab0d7c001c0c2fc68600e5
Maciej <macosspr@hotmail.com>
2016-11-06 12:27:50 +0100

    saS

--- source/AppMainBundle.php
+++ source/AppMainBundle.php
@@ -9,7 +9,7 @@ class AppMainBundle extends Bundle
     
     public function getParent()
     {
-        return 'FOSUserBundleAlaOh wchapanieldad FOSUserBundleAla';
+        return 'FOSUserdasdadBundleAlaOh wchapanieldad FOSUserBundleAla';
     }
     
     public function getParent2()
